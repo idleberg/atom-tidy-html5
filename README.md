2fd6a618d7d4dabe344ea06a8abca828cc5a1f2c
idleberg <jan@fragnebenan.com>
2018-06-26 09:23:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# tidy-html5
+
+[![apm](https://img.shields.io/apm/l/tidy-html5.svg?style=flat-square)](https://atom.io/packages/tidy-html5)
+[![apm](https://img.shields.io/apm/v/tidy-html5.svg?style=flat-square)](https://atom.io/packages/tidy-html5)
+[![apm](https://img.shields.io/apm/dm/tidy-html5.svg?style=flat-square)](https://atom.io/packages/tidy-html5)
+[![CircleCI](https://img.shields.io/circleci/project/idleberg/atom-tidy-html5.svg?style=flat-square)](https://circleci.com/gh/idleberg/atom-tidy-html5)
+
+# Description
+
+Run tidy over active HTML document or selections
+
+## Installation
+
+### apm
+
+Install `tidy-html5` from Atom's [Package Manager](http://flight-manual.atom.io/using-atom/sections/atom-packages/) or the command-line equivalent:
+
+`$ apm install tidy-html5`
+
+### Using Git
+
+Change to your Atom packages directory:
+
+**Windows**
+
+```cmd
+$ cd %USERPROFILE%\.atom\packages
+```
+
+**Linux & macOS**
+
+```bash
+$ cd ~/.atom/packages/
+```
+
+Clone the repository as `tidy-html5`:
+
+```bash
+$ git clone https://github.com/idleberg/atom-tidy-html5 tidy-html5
+```
+
+## Usage
+
+This package exposes two commands in the editor:
+
+- `Tidy: Document`
+- `Tidy: Selection`
+
+## License
+
+This work is licensed under the [MIT License](LICENSE)
---
+++ src/tidy.ts
@@ -0,0 +1,85 @@
+import { CompositeDisposable } from 'atom';
+import { tidy_html5 as tidy } from 'tidy-html5';
+
+module.exports = {
+  config: {},
+  subscriptions: null,
+  defaultOptions: {
+    'quiet': true,
+    'show-body-only': true,
+    'show-errors': 0,
+    'show-warnings': false
+  },
+
+  activate(): void {
+    // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
+    this.subscriptions = new CompositeDisposable;
+
+    // Register command that displays Hello World
+    this.subscriptions.add(atom.commands.add('atom-workspace', {
+      'tidy:document': () => this.tidyDocument(),
+      'tidy:selection': () => this.tidySelection()
+    }));
+  },
+
+  deactivate(): void {
+    this.subscriptions.dispose();
+  },
+
+  tidyDocument(): void {
+    const editor = atom.workspace.getActiveTextEditor();
+    if (editor == null) {
+      return atom.beep();
+    }
+
+    const scopeName = editor.getGrammar().scopeName;
+    if (!scopeName.startsWith('text.html')) {
+      return atom.beep();
+    }
+
+    let input, output;
+
+    input = editor.getText();
+    try {
+      output = this.tidy(input);
+      editor.setText(output);
+    } catch (e) {
+      console.error(e);
+    }
+  },
+
+  tidySelection(): void {
+    const editor = atom.workspace.getActiveTextEditor();
+    if (editor == null) {
+      return atom.beep();
+    }
+
+    const scopeName = editor.getGrammar().scopeName;
+    if (!scopeName.startsWith('text.html')) {
+      return atom.beep();
+    }
+
+    let input, output;
+
+    input = editor.getLastSelection().getText();
+    try {
+      output = this.tidy(input);
+      editor.setText(output);
+    } catch (e) {
+      console.error(e);
+    }
+  },
+
+  tidy(input: string): string {
+      let output;
+
+      try {
+        output = tidy(input, this.defaultOptions).trim();
+      } catch (e) {
+        console.error(e);
+        if (typeof input !== 'undefined') return input;
+      }
+
+      return output;
+  }
+};
